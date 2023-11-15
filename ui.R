62d27f38ba73c5f3307c1109db8cec83833ba092
Ashwin <75308021+Softwareguy3@users.noreply.github.com>
2023-11-15 21:35:38 +0530

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,29 @@
+library(shiny)
+
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Visualizing Cars Data"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      h3("Graph Configuration"),
+      p("Select a variable from the mtcars dataset for each aesthetic ",
+        "dimesion of the graph. Once all five aesthetics have been chosen, ",
+        "the plot of the data will be rendered accordingly"),
+      uiOutput("error"),
+      uiOutput("warning"),
+      uiOutput("xaxis"),
+      uiOutput("yaxis"),
+      uiOutput("color"),
+      uiOutput("shape"),
+      uiOutput("size"),
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+))
